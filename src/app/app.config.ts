2e5042315472a9fcf5c292f12534a79b98c66a36
Ibrahim Mohrah <ibrahimmohrah@gmail.com>
2025-12-04 16:03:22 +0100

    Initial commit frontend

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(HttpClientModule),]
+};
